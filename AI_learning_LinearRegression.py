fc86b5e636a469ae8b52e5b103f8cc7d83e255b1
Alqasem-Hindi <101357754+Alqasem-Hindi@users.noreply.github.com>
2022-06-25 14:44:15 -0400

    Add files via upload

--- AI_learning_LinearRegression.py
+++ AI_learning_LinearRegression.py
@@ -1,9 +1,14 @@
-from turtle import pd
 from sklearn.linear_model import LinearRegression
 from openpyxl import load_workbook
 import pandas as pd
 from sklearn import linear_model
-from sklearn.model_selection import train_test_split 
+import timeit
+import timeit
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm, projections
+from matplotlib.ticker import LinearLocator, FormatStrFormatter, MaxNLocator
 
 
 wb = load_workbook(r"C:\Users\alqas\Research\SHEtable2Vdc.xlsx")
@@ -24,21 +29,34 @@ for g in range(1,7):
         for row in col:
             z_t1[g-1].append(row)
 
+train_set_x, test_set_x = train_test_split(x_vdc1,test_size=.2,random_state=48)
+train_set_y, test_set_y = train_test_split(y_vdc2,test_size=.2,random_state=48)
+train_set_z1, test_set_z1 = train_test_split(z_t1[0],test_size=.2,random_state=48)
+train_set_z2, test_set_z2 = train_test_split(z_t1[1],test_size=.2,random_state=48)
+train_set_z3, test_set_z3 = train_test_split(z_t1[2],test_size=.2,random_state=48)
+train_set_z4, test_set_z4 = train_test_split(z_t1[3],test_size=.2,random_state=48)
+train_set_z5, test_set_z5 = train_test_split(z_t1[4],test_size=.2,random_state=48)
+train_set_z6, test_set_z6 = train_test_split(z_t1[5],test_size=.2,random_state=48)
+train_set_z = [train_set_z1,train_set_z2,train_set_z3,train_set_z4,train_set_z5,train_set_z6]
+test_set_z = [test_set_z1, test_set_z2,test_set_z3, test_set_z4,test_set_z5, test_set_z6]
+
 #reorganizing the data for ease of axis (not needed could just call the array itself)
-Data = {'x': x_vdc1,
-        'y': y_vdc2,
-        'z_1': z_t1[0],
-        'z_2': z_t1[1],
-        'z_3': z_t1[2],
-        'z_4': z_t1[3],
-        'z_5': z_t1[4],
-        'z_6': z_t1[5],
+Data = {'x': train_set_x,
+        'y': train_set_y,
+        'z_1': train_set_z1,
+        'z_2': train_set_z2,
+        'z_3': train_set_z3,
+        'z_4': train_set_z4,
+        'z_5': train_set_z5,
+        'z_6': train_set_z6,
         }
 df = pd.DataFrame(Data,columns=['x','y','z_1','z_2','z_3','z_4','z_5','z_6'])
 input=df[['x','y']]
 
 #in order to look at more outputs, just add the Z_number value for it to the list bellow
-output = df[['z_1','z_2','z_3']]
+
+z_variable_selection = 1 # change to mach z_value
+output = df[['z_1']]     # change to data set
 regression=linear_model.LinearRegression()
 regression.fit(input.values,output.values)
 
@@ -46,10 +64,31 @@ regression.fit(input.values,output.values)
 #print(regression.intercept_, regression.coef_)
 
 #in here are the input values used to predict the function
-new_x =112
-new_y = 120
-print(regression.predict([[new_x, new_y]]))
+start=timeit.default_timer()
+prediction_data = list()
+for x in range(len(test_set_x)):
+    prediction_data.append(float(regression.predict([[test_set_x[x], test_set_y[x]]])))
 
-# it is missing the test/train split
-# add a plot of z_1 from linear regression model vs actual
-# need a measure of error, performance: lets look at MSE first
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+
+summation = 0
+for test_value in range(len(test_set_x)):
+    summation = summation + (Data["z_1"][test_value]-prediction_data[test_value])**2 # to match above value
+MSE = 1/len(test_set_x)*summation
+print(MSE)
+
+fig = plt.figure()
+ax_real = fig.add_subplot(1,2,1,projection='3d')
+ax_prediction = fig.add_subplot(1,2,2, projection = '3d')
+surf_real = ax_real.plot_trisurf(test_set_x,test_set_y,test_set_z[z_variable_selection-1], cmap=cm.coolwarm,linewidth=0, antialiased=False)
+surf_prediction= ax_prediction.plot_trisurf(test_set_x, test_set_y, prediction_data, cmap=cm.coolwarm,linewidth=0, antialiased=False)
+cbaxes = fig.add_axes([0.1, 0.3, 0.01, 0.4]) 
+fig.colorbar(surf_real, shrink=0.5, aspect=5, cax = cbaxes )
+ax_real.set_xlabel('Vdc1, volts')
+ax_real.set_ylabel('Vdc2, volts')
+ax_real.set_zlabel('Angle, degrees')
+ax_prediction.set_xlabel('Vdc1, volts')
+ax_prediction.set_ylabel('Vdc2, volts')
+ax_prediction.set_zlabel('Angle, degrees')
+plt.show()
